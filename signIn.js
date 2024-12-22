de6726f3cf45e8dc84ae6a382bce2527573b7fc0
RIIKA <rishikam2005@gmail.com>
2024-12-23 00:01:48 +0530

    Add files via upload

---
+++ signIn.js
@@ -0,0 +1,10 @@
+
+document.getElementById('signinForm').addEventListener('submit', function(e) {
+    e.preventDefault();  
+
+    
+    console.log("Redirecting to clgproject.html...");
+    window.location.href = "clgproject.html";  
+});
+
+
